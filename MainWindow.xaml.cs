865fcfe89f172b99923e99f01e43393d795dbd59
archeryfox <archeryfox2005@gmail.com>
2023-01-24 01:17:37 +0300

    wtd is

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,17 +27,20 @@ namespace XO
             InitializeComponent();
 
         }
-        public int turn 
-        
+        public int turn = 0;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            turn = 0;
             List<string> Coll = XOs.Children.OfType<Button>().Select(x => x.Content.ToString()).ToList();
+            List<string> Help =new List<string>() { "", " ", "  ", "   ", "    ","      ", "       ","         ","               "};
             List<Button> Gr = XOs.Children.OfType<Button>().ToList();
             Debox.Text = null;
-            for (int i = 0; i < Gr.Count - 1; i++)
+            for (int i = 0; i < Gr.Count; i++)
             {
                 var ell = Gr[i];
-                ell.Content = " ";
+                ell.Content = Help[i];
+                ell.IsEnabled = true;
             }
             foreach (var item in Coll)
             {
@@ -46,14 +50,25 @@ namespace XO
         private void XO_Click(object sender, RoutedEventArgs e)
         {
             var b = sender as Button;
-
-            if (b.Content == "0")
+            if (b.Content == "X" || b.Content == "0")
             {
-                b.Content = "X";
+                MessageBox.Show("Тут уже сходили!");
             }
             else
             {
-                b.Content = "0";
+                if (turn % 2 == 0)
+                {
+                    b.Content = "X";
+                    turn++;
+                    IndTurn.Content = turn + " Нолики";
+                }
+                else
+                {
+                    b.Content = "0";
+                    turn++;
+                    IndTurn.Content = turn + " Крестики";
+                }
+
             }
             string[,] buts =
             {
@@ -61,27 +76,27 @@ namespace XO
                 {L.Content.ToString(),C.Content.ToString(),R.Content.ToString()},
                 {DL.Content.ToString(),D.Content.ToString(),DR.Content.ToString()}
             };
-            bool winU = (buts[0, 0] == buts[0, 1]) && (buts[0, 2] == buts[0, 1]);
-            bool winC = (buts[1, 0] == buts[1, 1]) && (buts[1, 2] == buts[1, 1]);
-            bool winD = (buts[2, 0] == buts[2, 1]) && (buts[2, 2] == buts[2, 1]);
-            var win = winC || winD || winU;
-            if ((buts[0, 0] == "") && (buts[0, 2] == "") && buts[0, 1] == "")
-            {
-                winU = false; 
+            /*bool winU = false;
+            bool winC;
+            bool winD;*/
+            var win =
+                   ((buts[0, 0] == buts[0, 1]) && (buts[0, 2] == buts[0, 0]))
+                || ((buts[0, 0] == buts[1, 0]) && (buts[1, 0] == buts[0, 0]))
+                || ((buts[1, 0] == buts[1, 1]) && (buts[1, 2] == buts[1, 0]))
+                || ((buts[0, 1] == buts[1, 1]) && (buts[1, 2] == buts[2, 1]))
+                || ((buts[2, 0] == buts[2, 1]) && (buts[2, 2] == buts[2, 0]));
 
-            }
-            if ((buts[1, 0] == "") && (buts[1, 2] == "") && buts[1, 1] == "")
-            {
-                winC = false;
 
-            }
-            if ((buts[2, 0] == "") && (buts[2, 2] == "") && buts[2, 1] == "")
-            {
-                winC = false;
+            
 
-            }
-            if (winC)
+            List<Button> Gr = XOs.Children.OfType<Button>().ToList();
+            if (win)
             {
+                for (int i = 0; i < Gr.Count; i++)
+                {
+                    var ell = Gr[i];
+                    ell.IsEnabled = false;
+                }
                 if (b.Content == "0")
                 {
                     MessageBox.Show($"Выиграли нолики!");
@@ -91,6 +106,7 @@ namespace XO
                     MessageBox.Show($"Выиграли крестики!");
                 }
             }
+            win = false;
 
         }
 
@@ -106,11 +122,8 @@ namespace XO
         private void XOs_Initialized(object sender, EventArgs e)
         {
             List<Button> Gr = XOs.Children.OfType<Button>().ToList();
-            for (int i = 0; i < Gr.Count; i++)
-            {
-                var ell = Gr[i];
-                ell.Content = ".";
-            }
+            const string _null = "";
+
         }
     }
 }
