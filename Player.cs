70fde4691cc482f16982977c29a9fcdcd2a22f56
archeryfox <archeryfox2005@gmail.com>
2023-01-25 22:17:50 +0300

    robot is stupid as me..

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XO
+{
+    public class Player
+    {
+        static public string sym = "";
+        static public bool X = true;
+        static Player()
+        {
+            sym = X ? "X" : "0";
+        }
+    }
+}
---
+++ Robot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XO
+{
+    public class Robot
+    {
+        private int randTouch;
+        public int RandTouch {
+            get { return randTouch =  new Random().Next(0,10); }
+        }
+        public static List<Button> RoboTurn(List<Button> bs)
+        {
+            Robot robot = new Robot();
+            robot.randTouch = robot.RandTouch;
+            bool f;
+            try
+            {
+                while ((bs[robot.randTouch].Content != "X" && bs[robot.randTouch].Content != "0"))
+                {
+                    bs[robot.randTouch].Content = "0";
+                    /*if (!(bs[robot.randTouch].Content != "X" && bs[robot.randTouch].Content != "0"))
+                    {
+                        break;
+                    }*/
+                }
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show($"{e}");
+            }
+            
+            //MessageBox.Show($"{f}");
+            return bs;
+        }
+    }
+}
