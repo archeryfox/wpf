bcfeeeae528a7e6feb01d6b7a7231c959c48b671
archeryfox <archeryfox2005@gmail.com>
2023-02-20 02:23:06 +0300

    страдания .mp3

--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Microsoft.Win32;
+using System.IO;
 
 namespace ToDo
 {
@@ -23,6 +26,61 @@ namespace ToDo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Calendr_Initialized(object sender, EventArgs e)
+        {
+            (sender as DatePicker).Text = $"{DateTime.Now}";
+        }
+
+        List<Task> TaskList = new List<Task>();
+        private void CreateTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task(TaskName.Text, TaskDescription.Text, Calendr.DisplayDate);
+            TaskList.Add(task);
+            ToDoList.ItemsSource = TaskList;
+            ToDoList.ItemsSource = ToDoList.Items.OfType<Task>().Select(x => x.Name).ToList();
+        }
+
+        private void SaveTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            string js = JsonConvert.SerializeObject(TaskList);
+            var way = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (!Directory.Exists(way + "\\Tasks") && !Task.FolderCreated)
+            {
+                if (MessageBox.Show("Задать папку для заметок?", "Mmm?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    saveFileDialog.Filter = "JSON file (*.json)|*.json";
+                    saveFileDialog.FileName = "Tasks.json";
+                    saveFileDialog.ShowDialog();
+                    Directory.CreateDirectory(way + "\\Tasks");
+                    Task.FolderPath = way + "\\Tasks\\";
+                    Task.FolderDefault();
+                    Task.FileName = saveFileDialog.SafeFileName;
+                    File.WriteAllText(Task.FolderPath + saveFileDialog.SafeFileName, js.Normalize());
+                    MessageBox.Show(Task.FolderPath + saveFileDialog.SafeFileName);
+                    return;
+                }
+            }
+            if (Directory.Exists(way + "\\Tasks") || Task.FolderCreated)
+            {
+                string fn = "Tasks.json";
+                Task.FolderDefault();
+                Task.FileName = fn;
+                Task.FolderPath = way + "\\Tasks\\";
+                File.WriteAllText(Task.FolderPath + fn, JsonConvert.SerializeObject(TaskList));
+            }
+        }
+
+        private void DeleteTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ToDoList.SelectedIndex.ToString());
+        }
+
+        private void Calendr_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(Calendr.Text);
         }
     }
 }
---
+++ ToDo/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class Task
+    {
+        public readonly string Name;
+        public readonly string Description;
+        public readonly DateTime DateTimeCreated;
+
+        public static string FileName = "Tasks.json";
+        public static string FolderPath { get; set; }
+        public static bool FolderCreated { get; private set; }
+        public Task(string name, string description, DateTime dateTimeCreated)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.DateTimeCreated = dateTimeCreated;
+        }
+       
+        static public void FolderDefault()
+        {
+            FolderCreated = true;
+        }
+    }
+}
